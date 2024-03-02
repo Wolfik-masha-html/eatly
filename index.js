4735615f69c034b9c933f9b328c910ffc18a4dd2
Wolfik-masha-html <sinatinskamaria@gmail.com>
2024-03-02 19:45:55 +0200

    animation

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   list.classList.toggle("active");
 });
+
+const opotins = {
+  root:null,
+  rootMargin:"0px",
+  treshold:0.5,
+}
+function callback(entries,observer){
+  entries.forEach(entry => {
+    if (entry.isInterSection) {
+      console.log("element видимий")
+    } else {
+      console.log("element невидимий")
+    }
+  });
+}
+const observer = new  IntersectionObserver(callback,options)
+const Targetelement = document.querySelector(".customers");
+observer.observe(Targetelement);
